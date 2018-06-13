d09b4135a1fa213f5804f4aa87d3be8fa49d8d44
David <davendntt@gmail.com>
2018-06-13 00:27:00 -0500

    change tag enable to disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My first app';
+  author = 'Dave To';
+  count = 0;
+  imgSource = './../favicon.ico';
+  enable = true;
 }
